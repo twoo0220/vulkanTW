3a3f8bd291746a49133fe291a6edde8c29eb52c1
twoo0220 <twoo0220@gmail.com>
2023-11-02 23:56:59 +0900

    vulkan 기본 셋팅

---
+++ vulkanTW/vulkanTW/main.cpp
@@ -0,0 +1,26 @@
+//#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
+#include <iostream>
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "vulkanTW", nullptr, nullptr);
+
+	uint32_t extensionCount = 0;
+	
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+}
